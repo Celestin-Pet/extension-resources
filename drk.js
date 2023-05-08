59287f6d71bdc0a81d020c7b3dd044698251dd62
peter-dev10 <tiegocelexq@gmail.com>
2023-05-08 14:20:57 +0200

    brouser extension

---
+++ drk.js
@@ -0,0 +1,6 @@
+chrome.runtime.sendMessage({name :"fetchWords"}, (response) => {
+console.log(response);
+document.querySelector('h1').innerHTML = response.word;
+document.querySelector('p').innerHTML = response.desc;
+
+});
